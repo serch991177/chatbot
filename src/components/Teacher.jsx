dd7b42207bd9b0fa79b8bc3f64e8927dec31b656
SERGIO ANDIA <sergioandia11@gmail.com>
2024-04-16 10:36:00 -0400

    agregado de movimiento en la boca y recursos glb

--- src/components/Teacher.jsx
+++ src/components/Teacher.jsx
@@ -1,9 +1,50 @@
+import { useAITeacher } from "@/hooks/useAITeacher";
 import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { MathUtils } from "three";
 
-export const teachers = ["Nanami","Naoki"];
+export const teachers = ["Nanami","Naoki","Octopus","Totoro","Biomech"];
 
 export const Teacher = ({teacher, ...props}) =>{
     const {scene} = useGLTF(`/models/Teacher_${teacher}.glb`);
+    //minute 35:05
+    //const {scene} = useGLTF(`/models/Octopus.glb`);
+    const currentMessage = useAITeacher((state) => state.currentMessage);
+
+    const lerpMorphTarget = (target,value,speed=0.01)=>{
+        scene.traverse((child) => {
+            if(child.isSkinnedMesh && child.morphTargetDictionary){
+                const index =child.morphTargetDictionary[target];
+                if(
+                    index === undefined ||
+                    child.morphTargetInfluences[index] === undefined
+                ){
+                    return;
+                }
+                child.morphTargetInfluences[index] = MathUtils.lerp(
+                    child.morphTargetInfluences[index],
+                    value,
+                    speed
+                );
+            }
+        });
+    };
+
+    useFrame(() =>{
+        for(let i = 0;i<=21; i++){
+            lerpMorphTarget(i,0,0.1);//reset morph targets
+        }
+        if(currentMessage && currentMessage.visemes && currentMessage.audioPlayer){
+            for(let i = currentMessage.visemes.length - 1;i>=0;i--){
+                const viseme = currentMessage.visemes[i];
+                if(currentMessage.audioPlayer.currentTime * 1000 >= viseme[0]){
+                    lerpMorphTarget(viseme[1],1,0.2);
+                    break;
+                };
+            };
+        }
+    });
+
     return  (
         <group {...props}>
             <primitive object={scene}/>
